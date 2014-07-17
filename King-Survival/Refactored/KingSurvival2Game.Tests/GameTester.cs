9419366cd85ff698da4e8db61728b5744da8c804
stoychevvasko <stoychev.vassil@gmail.com>
2014-07-17 11:58:36 +0300

    Unit Tests Added
    +for the GetYFromOriginalCoordinate() method

--- King-Survival/Refactored/KingSurvival2Game.Tests/GameTester.cs
+++ King-Survival/Refactored/KingSurvival2Game.Tests/GameTester.cs
@@ -24,7 +24,7 @@ namespace KingSurvival2Game.Tests
             GameUtilities.Interact(true, 0);
             Assert.AreEqual(KingSurvivalGame.GameIsFinished, true);
         }
-
+                
         /// <summary>
         /// test display facade game
         /// </summary>
@@ -188,6 +188,199 @@ namespace KingSurvival2Game.Tests
             try
             {
                 var result = GameUtilities.GetXFromOriginalCoordinate(15);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Assert.AreEqual(1, 1);
+                return;
+            }
+
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// tests TestGetXFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetXFromOriginalCoord09()
+        {
+            try
+            {
+                var result = GameUtilities.GetXFromOriginalCoordinate(-15);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Assert.AreEqual(1, 1);
+                return;
+            }
+
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// tests TestGetYFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetYFromOriginalCoord00()
+        {
+            var result = GameUtilities.GetYFromOriginalCoordinate(2);
+            if (result != 1)
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+
+        /// <summary>
+        /// tests TestGetYFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetYFromOriginalCoord01()
+        {
+            var result = GameUtilities.GetYFromOriginalCoordinate(3);
+            if (result != 4)
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+
+        /// <summary>
+        /// tests TestGetYFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetYFromOriginalCoord02()
+        {
+            var result = GameUtilities.GetYFromOriginalCoordinate(4);
+            if (result != 7)
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+
+        /// <summary>
+        /// tests TestGetYFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetYFromOriginalCoord03()
+        {
+            var result = GameUtilities.GetYFromOriginalCoordinate(5);
+            if (result != 10)
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+
+        /// <summary>
+        /// tests TestGetYFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetYFromOriginalCoord04()
+        {
+            var result = GameUtilities.GetYFromOriginalCoordinate(6);
+            if (result != 13)
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+
+        /// <summary>
+        /// tests TestGetYFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetYFromOriginalCoord05()
+        {
+            var result = GameUtilities.GetYFromOriginalCoordinate(7);
+            if (result != 16)
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+
+        /// <summary>
+        /// tests TestGetYFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetYFromOriginalCoord06()
+        {
+            var result = GameUtilities.GetYFromOriginalCoordinate(8);
+            if (result != 19)
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+
+        /// <summary>
+        /// tests TestGetYFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetYFromOriginalCoord07()
+        {
+            var result = GameUtilities.GetYFromOriginalCoordinate(9);
+            if (result != 22)
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+
+        /// <summary>
+        /// tests TestGetYFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetYFromOriginalCoord08()
+        {
+            try
+            {
+                var result = GameUtilities.GetYFromOriginalCoordinate(15);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Assert.AreEqual(1, 1);
+                return;
+            }
+
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// tests TestGetYFromOriginalCoordinate for expected behavior
+        /// </summary>
+        [TestMethod]
+        public void TestGetYFromOriginalCoord09()
+        {
+            try
+            {
+                var result = GameUtilities.GetYFromOriginalCoordinate(-5);
             }
             catch (ArgumentOutOfRangeException)
             {
